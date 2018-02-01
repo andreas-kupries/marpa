95859b510125099fa0a0e994b1814c485ee5b71e
aku <aku>
2018-02-01 20:36:21 +0000

    Testsuite work
    - Clean up of the support code, removed unused procedures.
    
    - Ensure that files are read with the proper encoding before fed into
      the string 'process' (See `fgetc` decl and use).
    
    - Allow setting of constraints, runtime-specific
    
    - Set __known bug__ constraint for eight y_* tests where rt-c currently
      diverges from rt-tcl (1).
    
    (1) These are all in the unicode/utf-8 handling, which differs between
        the available runtimes.
    
        * rt-tcl operates on chars and defers to Tcl's parsing of utf-8
          sequences.
    
        * rt-c OTOH operates on bytes, does its own utf-8 parsing, and is
          more strict (invalid sequences are a parse error).
    
        I have to see if I can define a char class (:invalid:) to contain
        the invalid sequences. Using that would allow me to either accept
        or discard them (depending on context).
    
        Similarly I might have to allow the class of surrogates (:Cs:), as
        acceptable characters, and as sequences for the characters past
        BMP. That would allow such characters even in Marpa limited to BMP.
    
        These are all things in the MarpaTcl core however, and not
        something specific to JSON. JSON just exposed the issues here.

--- languages/json/tests/support/textutils.tcl
+++ languages/json/tests/support/textutils.tcl
@@ -9,68 +9,9 @@ kt require support fileutil
 
 # Test suite support.
 # # ## ### ##### ######## #############
-## Normalize content of traces and other files, various forms and combinations
-## - Remove comment lines (#)
-## - Remove empty lines
-## - Remove leading whitespace (indentation)
-## - Remove trailing whitespace
 
-proc norm-trace {path} {
-    rejoin [strip-empty [strip-indent [strip-comments [strip-trailing [2lines $path]]]]]
-}
-
-proc norm-trace-gc {path} {
-    rejoin [strip-empty \
-		[strip-indent \
-		     [strip-gc-comments \
-			  [strip-comments [strip-trailing [2lines $path]]]]]]
-}
-
-proc rejoin {lines} {
-    join $lines \n
-}
-
-proc 2lines {path} {
-    split [string trimright [fget $path]] \n
-}
-
-proc fget {path} { fileutil::cat $path }
-
-proc strip-gc-comments {lines} {
-    lmap line $lines {
-	if {[string match {*GC comment*} $line]} continue
-	set line
-    }
-}
-
-proc strip-comments {lines} {
-    lmap line $lines {
-	if {![string match *failed* $line]} {
-	    regsub "^\\s*#.*\$" $line {} line
-	    regsub  "\\s+#.*\$" $line {} line
-	}
-	set line
-    }
-}
-
-proc strip-indent {lines} {
-    lmap line $lines { string trimleft $line }
-}
-
-proc strip-whitespace {lines} {
-    lmap line $lines { string trim $line }
-}
-
-proc strip-trailing {lines} {
-    lmap line $lines { string trimright $line }
-}
-
-proc strip-empty {lines} {
-    lmap line $lines {
-	if {[regexp {^\s*$} $line]} continue
-	set line
-    }
-}
+proc fget  {path}     { fileutil::cat $path }
+proc fgetc {path enc} { fileutil::cat -encoding $enc $path }
 
 # # ## ### ##### ######## #############
 return
