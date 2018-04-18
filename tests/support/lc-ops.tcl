b416da818ee3c9820ca3bcea00ad50968230941c
aku <aku>
2018-04-18 03:08:05 +0000

    Moved the main parts of `test/support/gen.tcl` into `bin/i-gen`. This new internal generator application uses the public `marpa-gen` as the underlying workhorse. The support code now invokes the internal generator instead of doing everything itself.
    With the operation confined to a child process the amount of memory
    taken by the test process' is limited to the generated package itself
    instead of all the packages needed to perform the generation.
    
    Further modified lexer-core to place the sets of tests for each
    variant grammar/lexer-action into a child process as well. This
    ensures that the memory needed by each variant grammar is limited to
    that child process, instead of accumulating in the controlling test
    process.
    
    __Attention__: This change requires a Kettle with support for
    `kt::sub`, added to Kettle with commit [ef384673c5] (2018-04-18
    02:28:17).

---
+++ tests/support/lc-ops.tcl
@@ -0,0 +1,61 @@
+
+# # ## ### ##### ######## ############# #####################
+## Test case support - Lexer creation and cleanup, data capture
+
+proc setup {} {
+    global TRACE
+    [gen cget cl] create LEX
+    set TRACE {}
+}
+
+proc cleanup {} {
+    global TRACE
+    LEX destroy
+    unset TRACE
+}
+
+proc capture {m args} {
+    if {$m ne "enter"} return
+    # args = list (list(symbol), list(value))
+    lassign $args symbols svalues
+    set svalues [lsort -uniq $svalues]
+    if {[llength $svalues] == 1} {
+	set svalues [lindex $svalues 0]
+    }
+    lappend ::TRACE $svalues
+    return
+}
+
+# # ## ### ##### ######## ############# #####################
+## Lexer class generation and cleanup
+
+proc lex-setup {lexaction} {
+    global clsave
+    set clsave [gen cget cl]
+    set grsave [gen cget gr]
+
+    set gr [td]/g_$lexaction
+    set cl ${clsave}-$lexaction
+
+    # Override the lexer semantics specified by the origin grammar
+    # with what we wish to test.
+    lappend map "lexeme default =\n  action => \[start,length,value\]"
+    lappend map "lexeme default = action => \[$lexaction\]"
+    fileutil::writeFile $gr [string map $map [fget $grsave]]
+
+    gen setup cl $cl gr $gr
+
+    # Cleanup of helper grammar
+    gen configure gr $grsave
+    file delete $gr
+    return
+}
+
+proc lex-cleanup {lexaction} {
+    global clsave
+    gen cleanup
+    gen configure cl $clsave
+    return
+}
+
+# # ## ### ##### ######## ############# #####################
