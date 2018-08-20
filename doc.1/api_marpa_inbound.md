01fdbffb7345ee3d2c61df89bcf7967fa9fbc808
Andreas Kupries <Andreas Kupries>
2018-08-20 23:16:49 +0000

    Reworked API of low-level inbound, documented start/stop location, and limit meanings. Updated tests. TODO: Update higher layers.

--- doc.1/api_marpa_inbound.md
+++ doc.1/api_marpa_inbound.md
@@ -2,25 +2,88 @@
 
 ## API signatures
 
-1. constructor (-> semantic-store, -> upstream)
-2. enter (string)
-3. read (chan)
-4. eof ()
+ 1. constructor (-> upstream)
 
-All but the constructor are driver methods which provide either more
-data to process (`enter`, `read`), or signal the end of the input
-(`eof`).
+ 2. enter (string ?from to?)	/-1, -2/
+ 3. read  (chan   ?from to?)	/-1, -2/
+ 4. enter-more (string)
+ 5. read-more  (chan)
+ 6. last
+ 7. location
+ 8. stop
+ 9. from     (pos ...)
+10. rewind   (delta)
+11. relative (delta)
+12. to       (pos)
+13. dont-stop
+14. limit    (delta)
+
+After the constructor we have 4 driver methods to provide initial and
+additional data to process. The drivers for the initial data further
+set up the stage's state with respect to start and stop locations.
+The remaining operations are accessors and modifiers for start and
+stop location to be called by event handlers during processing.
+
+# Start and stop locations
+
+These are engine internal location values which are not directly
+visible to the user. Other parts of the engine perform translations
+between thesen and the user-visible values, in either direction.
+
+Start location:
+
+	Initial location of the cursor to process characters. As the
+	loop invariant at loop entry is to have the cursor pointing to
+	the _last_ processed character the default value of -1 means
+	that processing will start with the first character of the
+	input, at index 0.
+
+	The start location can range from -1 to input.size-1. The last
+	value means that no character will be processed at all because
+	the character to process is after the end of the input,
+	i.e. does not exist.
+
+Stop location:
+
+	The index of the character to stop the engine at, after
+	processing that character. A value of -1 thus means to stop
+	engine after processing the non-existent character before the
+	first character, and thus equivalently before processing the
+	first character of the input at index 0.
+
+	Furthermore the default value of -2 thus means to stop before
+	processing the non-existing character before the first
+	character. As that index is never reached in regular operation
+	the engine will actually not stop. This value therefore is the
+	in-band signal for a non-stopping engine.
+
+	The stop location can range from -2 to input.size-1. The
+	latter means that the engine stops just after the last
+	character, before attempting to process the non-existing
+	character after the end of the input.
+
+Limit semantics:
+
+	Limit X says to stop the engine after processing the next X
+	characters, relative to the current cursor location.
+
+	As the current cursor C is just before the first next
+	character to process, the new stop location S is `C + X`,
+	stopping the engine just before `C + X + 1`.
+
+	Example:
+	ix = 0 1 2 3 4 5 6 7 8 9 ...
+	IN = a l p h a n u m e r i c
+	C  = 3 ----^ | | | | |
+	L  = 5 ------/-/-/-/-/
+	S  = 8, stop before 9.
 
 ## API call ordering
 
-Specified via regular expression: `1[23]*4`
+Specified via regular expression: `1[23][45,9-14]*`.
+The APIs 6-8 may occur anywhere after `1`.
 
 ## API expectations on linked objects
-
-   * semantic-store
-     1. put (data) -> sem-value-id
-
-     Example: `marpa::tvstore`
 
    * upstream
      1. enter (char, sem-value-id)
@@ -53,7 +116,6 @@ Driver  Inbound SemStore        Upstream
 |       +-enter----------------->
 |       *       |               |
 |       |       |               |
-+-eof--->       |               |
 |       +-eof------------------->
 |       |       |               |
 ```
@@ -72,7 +134,6 @@ Driver  Inbound SemStore        Upstream
 |       +-enter----------------->
 |       *       |               |
 |       |       |               |
-+-eof--->       |               |
 |       +-eof------------------->
 |       |       |               |
 ```
--- runtime-tcl/inbound.tcl
+++ runtime-tcl/inbound.tcl
@@ -66,6 +66,7 @@ oo::class create marpa::inbound {
     variable mytext         ; # Physical input stream
     			      # (list! of characters)
     variable mymax          ; # Location triggering EOF
+    variable mysentinel     ; # Location triggering overrun
 
     # API:
     #  1 cons       (postprocessor)    - Create, link
@@ -101,6 +102,8 @@ oo::class create marpa::inbound {
 				# before the first character (of
 				# nothing).
 	set mystoplocation -2 ; # Where to stop the engine, nowhere.
+	set mysentinel     -1
+	set mymax          -1
 
 	# Attach ourselves to the postprocessor, as its input
 	Forward input: [self]
@@ -124,7 +127,6 @@ oo::class create marpa::inbound {
 
     method from {pos args} {
 	debug.marpa/inbound {[debug caller] | }
-	incr pos -1
 	set mylocation $pos
 	foreach delta $args { incr mylocation $delta }
 	return
@@ -144,7 +146,7 @@ oo::class create marpa::inbound {
 
     method stop {} {
 	debug.marpa/inbound {[debug caller] | }
-	if {$mystoplocation < 0} { return {} }
+	if {$mystoplocation < -1} { return {} }
 	return $mystoplocation
     }
 
@@ -164,12 +166,11 @@ oo::class create marpa::inbound {
 	# assert delta > 0
 	debug.marpa/inbound {[debug caller] | }
 	set  mystoplocation $mylocation
-	incr mystoplocation
 	incr mystoplocation $delta
 	return
     }
 
-    method read {chan {from -1} {to -1}} {
+    method read {chan {from -1} {to -2}} {
 	debug.marpa/inbound {[debug caller] | }
 	# Read entire channel into memory for processing
 	my enter [read $chan] $from $to
@@ -190,7 +191,21 @@ oo::class create marpa::inbound {
 
 	set  max [llength $mytext]
 	incr max -1
-	set mymax $max
+	set  mysentinel $max
+	set  mymax      $max
+
+	# Example input, with secondary data (15 chars)
+	#             0 1 2 3 4 5 6 7 8 9 A B C  D E
+	# text     = {a l p h a n u m e r i c \0 u m}
+	# max      = 11 --------------------^
+	# sentinel = 14 ---------------------------^
+	#
+	# max = Trigger EOF when this (@11) is the previous processed
+	#       character.
+	#
+	# sentinel = Trigger overrun when this (@14) is the previous
+	#            processed character and engine was not stopped
+	#            by EOF or stop marker.
 
 	debug.marpa/inbound {[debug caller 1] | DO _______________________________________ /START}
 
@@ -236,6 +251,11 @@ oo::class create marpa::inbound {
 		Forward signal-stop ;# notify gate
 	    }
 
+	    if {$mylocation == $mysentinel} {
+		Forward signal-overrun
+		return -code error "Input overrun after $mysentinel"
+	    }
+
 	    incr mylocation
 
 	    set ch [lindex $mytext $mylocation]
@@ -260,8 +280,10 @@ oo::class create marpa::inbound {
     method enter-more {string} {
 	debug.marpa/inbound {[debug caller 1] | }
 	set start [llength $mytext]
-	incr start ;# ----v point past the \0
+	#   \-----------\ point @ separator, resume after
 	lappend mytext \0 {*}[split $string {}]
+	set  mysentinel [llength $mytext]
+	incr mysentinel -1
 	# Notes
 	# - The {*} will put the entire string on the Tcl stack before
 	#   it becomes part of the input buffer.
@@ -269,9 +291,9 @@ oo::class create marpa::inbound {
 	#   by `enter` to detect the end of the primary input.
 	# - The new input will only be processed by moving explictly
 	#   into its region. Processing it will not trigger EOI.
-	# - The \0 separator ensures that positioning to the start of
-	#   the first of the secondary inputs without triggering the
-	#   eof condition in `enter` when we move to it.
+	# - The \0 separator ensures that positioning the cursor to
+	#   just before the first of the secondary inputs will not
+	#   trigger the EOF condition in `enter`.
 	return $start
     }
 
@@ -329,7 +351,7 @@ oo::class create marpa::inbound::sequencer {
     # # -- --- ----- -------- -------------
     ## Checked API methods
 
-    method enter {string {from -1} {to -1}} {
+    method enter {string {from -1} {to -2}} {
 	my __Init
 	my __Fail running ! "Unable to process input after start" START
 	my __Fail done    ! "Unable to process input after EOF" EOF
