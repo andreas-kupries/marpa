c7a46f69038952c06d187e40bf9c4a01b4bc25c3
Andreas Kupries <Andreas Kupries>
2018-08-30 00:07:06 +0000

    Start some cleanup of testsuite, some simplifications for var handling of the loop helpers ...

--- tests/support/grammars.tcl
+++ tests/support/grammars.tcl
@@ -15,35 +15,23 @@ kt require support fileutil
 kt require support lambda
 kt local   support marpa::unicode
 
-proc test-grammar-files {key __ iv varname script} {
-    upvar 1 $iv id $varname iter
-
-    set gdir [grdir]
-
-    fileutil::traverse T $gdir -filter [lambda {key path} {
-	string equal $key [file tail $path]
-    } $key]
-    set files [lreverse [T files]]
-    # Reversal because T returns files in reverse lexicographical order.
-    # This is also the reason for not using (T foreach).
-    T destroy
-    foreach iter $files {
-	set stem  [file dirname $iter]
-	set id    [string map {/ ,} [fileutil::stripPath $gdir $stem]]
-	set code [catch { uplevel 1 $script } msg]
-	switch -exact -- $code {
-	    0 {}
-	    1 { return -code error $msg }
-	    2 { return }
-	    3 { break }
-	    4 {}
-	}
-    }
+proc test-grammar-file-format {name key} {
+    set format %0[string length [test-grammar-file-count $key]]d
+    proc $name {id} "format $format \$id"
     return
 }
 
-proc test-grammar-map {key __ iv varname basevar script} {
-    upvar 1 $iv id $varname iter $basevar stem
+proc test-grammar-file-count {key} {
+    fileutil::traverse T [grdir] -filter [lambda {key path} {
+	string equal $key [file tail $path]
+    } $key]
+    set count [llength [T files]]
+    T destroy
+    return $count
+}
+
+proc test-grammar-files {key __ sv iv varname script} {
+    upvar 1 $sv serial $iv id $varname iter
 
     set gdir [grdir]
 
@@ -54,9 +42,12 @@ proc test-grammar-map {key __ iv varname basevar script} {
     # Reversal because T returns files in reverse lexicographical order.
     # This is also the reason for not using (T foreach).
     T destroy
+
+    set serial 0
     foreach iter $files {
-	set stem   [file dirname $iter]
-	set id     [string map {/ ,} [fileutil::stripPath $gdir $stem]]
+	set stem  [file dirname $iter]
+	set id    [string map {/ ,} [fileutil::stripPath $gdir $stem]]
+	incr serial
 	set code [catch { uplevel 1 $script } msg]
 	switch -exact -- $code {
 	    0 {}
@@ -66,6 +57,40 @@ proc test-grammar-map {key __ iv varname basevar script} {
 	    4 {}
 	}
     }
+
+    unset serial id iter
+    return
+}
+
+proc test-grammar-map {key __ sv iv varname basevar script} {
+    upvar 1 $sv serial $iv id $varname iter $basevar stem
+
+    set gdir [grdir]
+
+    fileutil::traverse T $gdir -filter [lambda {key path} {
+	string equal $key [file tail $path]
+    } $key]
+    set files [lreverse [T files]]
+    # Reversal because T returns files in reverse lexicographical order.
+    # This is also the reason for not using (T foreach).
+    T destroy
+
+    set serial 0
+    foreach iter $files {
+	set stem   [file dirname $iter]
+	set id     [string map {/ ,} [fileutil::stripPath $gdir $stem]]
+	incr serial
+	set code [catch { uplevel 1 $script } msg]
+	switch -exact -- $code {
+	    0 {}
+	    1 { return -code error $msg }
+	    2 { return }
+	    3 { break }
+	    4 {}
+	}
+    }
+
+    unset serial id iter stem
     return
 }
 
